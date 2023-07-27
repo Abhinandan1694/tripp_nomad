1df2ea964929330924843bdf835ffb99e6ad3e40
Abhinandan1694 <abhinandan.j.a.09@gmail.com>
2023-07-27 22:36:06 +0530

    cmt

---
+++ src/Components/MenuItems.js
@@ -0,0 +1,33 @@
+
+export const MenuItems = [
+    {
+        title : "Home",
+        url : "/",
+        cName : "nav-item",
+        icon : "fas fa-house-user fa-2xl"
+    },
+    {
+        title : "About",
+        url : "/about",
+        cName : "nav-item",
+        icon : "fas fa-circle-info fa-2xl"
+    },
+    {
+        title : "Service",
+        url : "/service",
+        cName : "nav-item",
+        icon : "fas fa-briefcase fa-2xl"
+    },
+    {
+        title : "Contact",
+        url : "/contact",
+        cName : "nav-item",
+        icon : "fas fa-address-book fa-2xl"
+    },
+    {
+        title : "Sign Up",
+        url : "/signup",
+        cName : "signup-btn",
+    }
+]
+
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,26 @@
-import './navbar.css'
-import React from 'react'
-
+import "./navbar.css";
+import React from "react";
+import {MenuItems} from './MenuItems'
+import { padding } from "@mui/system";
 
 const Navbar = () => {
-
-
   return (
     <React.Fragment>
       <nav className="navbar">
-        <h1 className="navbar-brand">
-          Trippy
-        </h1>
+        <h1 className="navbar-brand">Trippy</h1>
         <ul className="navbar-menu">
-          <li className="navbar-item">
-           <i className="fa-duotone fa-house-user fa-2xs"></i>Home
-          </li>
+
+          {MenuItems.map((item, index) => {
+            return <li key={index} className={item.cName}>
+                 <i className={item.icon}>
+                <span className="icon-word">{item.title}</span></i>
+            </li>
+          })}
+
         </ul>
       </nav>
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Navbar;
